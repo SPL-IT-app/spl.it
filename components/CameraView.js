324636c889273d13fd41c2c6f2620ac9b5cbe64c
Julianne Crawford <julianne.marik@gmail.com>
2018-11-13 17:06:27 -0600

    working on cloud vision api

--- components/CameraView.js
+++ components/CameraView.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { Camera, Permissions } from 'expo';
+import vision from 'react-native-cloud-vision-api';
 
 export default class CameraView extends React.Component {
   state = {
@@ -14,10 +15,35 @@ export default class CameraView extends React.Component {
   }
 
   takePicture = async () => {
-    let photo = await this.camera.takePictureAsync({base64: true, })
 
-    console.log({photo: photo.base64}, '<<<< photo object')
-  }
+    /**
+     * TODO(developer): Uncomment the following line before running the sample.
+     */
+    // const fileName = 'Local image file, e.g. /path/to/image.png';
+
+    // Performs text detection on the local file
+    let photo = await this.camera.takePictureAsync({ base64: true });
+    console.log("photo taken")
+    try {
+      vision.init({ auth: process.env.API_KEY });
+      const req = new vision.Request({
+        image: new vision.Image({
+          base64: photo.base64,
+        }),
+        features: [
+          new vision.Feature('TEXT_DETECTION', 4)
+        ],
+      });
+      vision.annotate(req).then((res) => {
+        // handling response
+        console.log("res", res)
+        console.log(JSON.stringify(res.responses))
+      }).catch(err => console.log(err))
+    } catch (err) {
+      console.log("some error happened")
+      console.error(err);
+    }
+  };
 
   render() {
     const { hasCameraPermission } = this.state;
@@ -28,13 +54,20 @@ export default class CameraView extends React.Component {
     } else {
       return (
         <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type} ref={ref => {this.camera = ref}}>
+          <Camera
+            style={{ flex: 1 }}
+            type={this.state.type}
+            ref={ref => {
+              this.camera = ref;
+            }}
+          >
             <View
               style={{
                 flex: 1,
                 backgroundColor: 'transparent',
                 flexDirection: 'row',
-              }}>
+              }}
+            >
               <TouchableOpacity
                 style={{
                   flex: 0.1,
@@ -42,16 +75,19 @@ export default class CameraView extends React.Component {
                   alignItems: 'center',
                 }}
                 onPress={() => {
-                 /*  this.setState({
+                  /*  this.setState({
                     type: this.state.type === Camera.Constants.Type.back
                       ? Camera.Constants.Type.front
                       : Camera.Constants.Type.back,
                   }); */
-                  this.takePicture()
-                }}>
+                  this.takePicture();
+                }}
+              >
                 <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Take Photo{' '}
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}
+                >
+                  {' '}
+                  Take Photo{' '}
                 </Text>
               </TouchableOpacity>
             </View>
