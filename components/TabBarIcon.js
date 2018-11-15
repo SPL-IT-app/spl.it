e478d3e09e593c7aba1b68e2410c4116a74d8f79
Mustafa Dane <mustafadane@gmail.com>
2018-11-15 15:58:55 -0600

    custom navbar styling added

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -8,17 +8,17 @@ import { Footer, FooterTab, Text } from 'native-base'
 export default class TabBarIcon extends React.Component {
   render() {
     return (
-      // <Icon.Ionicons
-      //   name={this.props.name}
-      //   size={26}
-      //   style={{ marginBottom: -3 }}
-      //   color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-      // />
-      <Footer>
-        <FooterTab>
-          <Text>{this.props.name}</Text>
-        </FooterTab>
-      </Footer>
+      <Icon.Ionicons
+        name={this.props.name}
+        size={26}
+        style={{ marginBottom: -3 }}
+        color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      />
+      // <Footer>
+      //   <FooterTab>
+      //     <Text>{this.props.name}</Text>
+      //   </FooterTab>
+      // </Footer>
     );
   }
 }
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { Footer, FooterTab, Text, Button, Icon } from 'native-base'
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
@@ -13,50 +14,89 @@ const HomeStack = createStackNavigator({
   Home: HomeScreen,
 });
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
+// HomeStack.navigationOptions = {
+//   tabBarLabel: 'Home',
+//   tabBarIcon: ({ focused }) => (
+//     // <FooterTab>
+//     //   <Text>HOME</Text>
+//     // </FooterTab>
+//     <TabBarIcon
+//       focused={focused}
+//       name={
+//         Platform.OS === 'ios'
+//           ? `ios-information-circle${focused ? '' : '-outline'}`
+//           : 'md-information-circle'
+//       }
+//     />
+//   ),
+// };
 
 const LinksStack = createStackNavigator({
   Links: LinksScreen,
 });
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
-};
+// LinksStack.navigationOptions = {
+//   tabBarLabel: 'Links',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+//     />
+//   ),
+// };
 
 const SettingsStack = createStackNavigator({
   Settings: SettingsScreen,
 });
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
-};
+// SettingsStack.navigationOptions = {
+//   tabBarLabel: 'Settings',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+//     />
+//   ),
+// };
 
 export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
   SettingsStack,
+}, {
+  tabBarComponent: props => {
+    return (
+      <Footer>
+        <FooterTab>
+          <Button
+            active={props.navigation.history === 'HomeStack'}
+            onPress={() => props.navigation.navigate("HomeStack")}
+          >
+            <Icon name='calendar' />
+            <Text>
+              Events
+            </Text>
+          </Button>
+          <Button
+            // active={props.navigationState.index === 0}
+            onPress={() => props.navigation.navigate("LinksStack")}
+          >
+            <Icon type= 'MaterialCommunityIcons' name='account' />
+            <Text>
+              Profile
+            </Text>
+          </Button>
+          <Button
+            // active={props.navigationState.index === 0}
+            onPress={() => props.navigation.navigate("SettingsStack")}
+          >
+            <Icon name='more' />
+            <Text>
+              More
+            </Text>
+          </Button>
+        </FooterTab>
+      </Footer>
+    )
+  }
 });
