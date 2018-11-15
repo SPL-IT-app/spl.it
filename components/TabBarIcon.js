9affcd3693edf397e1f5db84bccd3d065b882bac
Mustafa Dane <mustafadane@gmail.com>
2018-11-15 14:35:15 -0600

    Roboto font async loading added

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -3,15 +3,22 @@ import { Icon } from 'expo';
 
 import Colors from '../constants/Colors';
 
+import { Footer, FooterTab, Text } from 'native-base'
+
 export default class TabBarIcon extends React.Component {
   render() {
     return (
-      <Icon.Ionicons
-        name={this.props.name}
-        size={26}
-        style={{ marginBottom: -3 }}
-        color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-      />
+      // <Icon.Ionicons
+      //   name={this.props.name}
+      //   size={26}
+      //   style={{ marginBottom: -3 }}
+      //   color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      // />
+      <Footer>
+        <FooterTab>
+          <Text>{this.props.name}</Text>
+        </FooterTab>
+      </Footer>
     );
   }
 }
