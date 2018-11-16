d38c3de19dc71aa3d24f5e0bdb1983bf84e36762
Brittany Hill <brit.hill10@gmail.com>
2018-11-16 16:02:31 -0600

    add scrollview

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -11,7 +11,7 @@ import {
     Icon
 } from 'native-base'
 
-import { StyleSheet, TouchableOpacity } from 'react-native'
+import { StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
 
 const styles = StyleSheet.create({
     container: {
@@ -32,7 +32,8 @@ class SignUpScreen extends React.Component {
             lastName: '',
             email: '',
             phone: '',
-            password: ''
+            password: '',
+            username: '',
         }
     }
 
@@ -45,7 +46,7 @@ class SignUpScreen extends React.Component {
             } else {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
                     .then(user => {
-                        const { firstName, lastName, phone } = this.state
+                        const { firstName, lastName, phone, username } = this.state
                         firebase
                             .database()
                             .ref('users')
@@ -55,6 +56,15 @@ class SignUpScreen extends React.Component {
                                     lastName,
                                     phone,
                                     email
+                                }
+                            })
+                        firebase
+                            .database()
+                            .ref('profiles')
+                            .update({
+                                [user.user.uid]: {
+                                    imageUrl: 'https://bit.ly/2qQRtn6',
+                                    username
                                 }
                             })
                         getUser({ id: user.user.uid })
@@ -68,69 +78,82 @@ class SignUpScreen extends React.Component {
 
     render() {
         return (
-            <Container style={styles.container}>
-                <View style={{ marginLeft: 10 }}>
-                    <TouchableOpacity><Text onPress={() => this.props.navigation.navigate('Login')}>Back</Text></TouchableOpacity>
-                </View>
-                <Form>
-                    <Item floatingLabel>
-                        <Label>First Name</Label>
-                        <Input
-                            value={this.state.firstName}
-                            onChangeText={(firstName) => this.setState({ firstName })}
-                        />
-                    </Item >
-                    <Item floatingLabel>
-                        <Label>Last Name</Label>
-                        <Input
-                            value={this.state.lastName}
-                            onChangeText={(lastName) => this.setState({ lastName })}
-                        />
-                    </Item >
-                    <Item floatingLabel>
-                        <Label>Email</Label>
-                        <Input
-                            keyboardType='email-address'
-                            value={this.state.email}
-                            autoCorrect={false}
-                            autoCapitalize='none'
-                            onChangeText={(email) => this.setState({ email })}
-                        />
-                    </Item >
-                    <Item floatingLabel>
-                        <Label>Phone</Label>
-                        <Input
-                            returnKeyType='done'
-                            returnKeyLabel={true}
-                            keyboardType='phone-pad'
-                            maxLength={11}
-                            dataDetectorTypes='phoneNumber'
-                            value={this.state.phone}
-                            autoCorrect={false}
-                            autoCapitalize='none'
-                            onChangeText={(phone) => this.setState({ phone })}
-                        />
-                    </Item >
-                    <Item floatingLabel>
-                        <Label>Password</Label>
-                        <Input
-                            value={this.state.password}
-                            secureTextEntry={true}
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            onChangeText={(password) => this.setState({ password })}
-                        />
-                    </Item>
-                    <Button
-                        full
-                        rounded
-                        style={{ marginTop: 10 }}
-                        onPress={() => this.signUpUser(this.state.email, this.state.password)}
-                    >
-                        <Text>Sign Up</Text>
-                    </Button>
-                </Form>
-            </Container>
+            <ScrollView>
+                <Container style={styles.container}>
+                    <View style={{ marginLeft: 10 }}>
+                        <TouchableOpacity><Text onPress={() => this.props.navigation.navigate('Login')}>Back</Text></TouchableOpacity>
+                    </View>
+                    <Form>
+                        <Item floatingLabel>
+                            <Label>Username</Label>
+                            <Input
+                                autoCorrect={false}
+                                autoCapitalize='none'
+                                value={this.state.username}
+                                onChangeText={(username) => this.setState({ username })}
+                            />
+                        </Item >
+                        <Item floatingLabel>
+                            <Label>First Name</Label>
+                            <Input
+                                value={this.state.firstName}
+                                autoCorrect={false}
+                                onChangeText={(firstName) => this.setState({ firstName })}
+                            />
+                        </Item >
+                        <Item floatingLabel>
+                            <Label>Last Name</Label>
+                            <Input
+                                value={this.state.lastName}
+                                autoCorrect={false}
+                                onChangeText={(lastName) => this.setState({ lastName })}
+                            />
+                        </Item >
+                        <Item floatingLabel>
+                            <Label>Email</Label>
+                            <Input
+                                keyboardType='email-address'
+                                value={this.state.email}
+                                autoCorrect={false}
+                                autoCapitalize='none'
+                                onChangeText={(email) => this.setState({ email })}
+                            />
+                        </Item >
+                        <Item floatingLabel>
+                            <Label>Phone</Label>
+                            <Input
+                                returnKeyType='done'
+                                returnKeyLabel={true}
+                                keyboardType='phone-pad'
+                                maxLength={11}
+                                dataDetectorTypes='phoneNumber'
+                                value={this.state.phone}
+                                autoCorrect={false}
+                                autoCapitalize='none'
+                                onChangeText={(phone) => this.setState({ phone })}
+                            />
+                        </Item >
+                        <Item floatingLabel>
+                            <Label>Password</Label>
+                            <Input
+                                value={this.state.password}
+                                secureTextEntry={true}
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                onChangeText={(password) => this.setState({ password })}
+                            />
+                        </Item>
+                        <Button
+                            full
+                            rounded
+                            style={{ marginTop: 10 }}
+                            onPress={() => this.signUpUser(this.state.email, this.state.password)}
+                        >
+                            <Text>Sign Up</Text>
+                        </Button>
+                    </Form>
+                </Container>
+            </ScrollView>
         )
     }
 }
