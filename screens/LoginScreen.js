a54ff3f3eb21187dd8632e4db2ad3ac1e6f96c00
Julianne Crawford <julianne.marik@gmail.com>
2018-11-17 10:36:12 -0600

    s replaced by dollar sign in logo

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,13 +7,13 @@ import { connect } from 'react-redux';
 import { StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
-    root: {
-        display: 'flex',
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
+  root: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     width: '95%',
@@ -23,11 +23,11 @@ const styles = StyleSheet.create({
   text: {
     textAlign: 'center',
     fontWeight: '200',
-    letterSpacing: 2
+    letterSpacing: 2,
   },
   signUpText: {
     marginTop: 20,
-    letterSpacing: 2
+    letterSpacing: 2,
   },
   signUp: {
     fontWeight: 'bold',
@@ -44,8 +44,8 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   form: {
-      width: '100%',
-  }
+    width: '100%',
+  },
 });
 
 class LoginScreen extends React.Component {
@@ -78,51 +78,54 @@ class LoginScreen extends React.Component {
 
   render() {
     return (
-        <Container style={styles.root}>
-      <Container style={styles.container}>
-        <Text style={styles.logoText}>SPL/IT</Text>
-        <Form style={styles.form}>
-          <Item floatingLabel>
-            <Label>Email</Label>
-            <Input
-              keyboardType="email-address"
-              value={this.state.email}
-              autoCorrect={false}
-              autoCapitalize="none"
-              onChangeText={email => this.setState({ email })}
-            />
-          </Item>
-          <Item floatingLabel>
-            <Label>Password</Label>
-            <Input
-              value={this.state.password}
-              secureTextEntry={true}
-              autoCapitalize="none"
-              autoCorrect={false}
-              onChangeText={password => this.setState({ password })}
-            />
-          </Item>
-          <Button
-            warning
-            block
-            style={styles.button}
-            onPress={() =>
-              this.loginUser(this.state.email, this.state.password)
-            }
-          >
-            <Text style={styles.text}> LOGIN </Text>
-          </Button>
-        </Form>
-        <View>
-          <Text style={styles.signUpText}> New to SPL/IT? <Text
-            style={styles.signUp}
-            onPress={() => this.props.navigation.navigate('SignUp')}
-          >
-            Sign Up
-          </Text></Text>
-
-        </View>
-      </Container>
+      <Container style={styles.root}>
+        <Container style={styles.container}>
+          <Text style={styles.logoText}>$PL/IT</Text>
+          <Form style={styles.form}>
+            <Item floatingLabel>
+              <Label>Email</Label>
+              <Input
+                keyboardType="email-address"
+                value={this.state.email}
+                autoCorrect={false}
+                autoCapitalize="none"
+                onChangeText={email => this.setState({ email })}
+              />
+            </Item>
+            <Item floatingLabel>
+              <Label>Password</Label>
+              <Input
+                value={this.state.password}
+                secureTextEntry={true}
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={password => this.setState({ password })}
+              />
+            </Item>
+            <Button
+              warning
+              block
+              style={styles.button}
+              onPress={() =>
+                this.loginUser(this.state.email, this.state.password)
+              }
+            >
+              <Text style={styles.text}> LOGIN </Text>
+            </Button>
+          </Form>
+          <View>
+            <Text style={styles.signUpText}>
+              {' '}
+              New to SPL/IT?{' '}
+              <Text
+                style={styles.signUp}
+                onPress={() => this.props.navigation.navigate('SignUp')}
+              >
+                Sign Up
+              </Text>
+            </Text>
+          </View>
+        </Container>
       </Container>
     );
   }
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,158 +1,202 @@
 import React from 'react';
 import {
-    Button,
-    Form,
-    Label,
-    Item,
-    Input,
-    Text,
-    View,
-    Icon,
-} from 'native-base'
+  Button,
+  Form,
+  Label,
+  Item,
+  Input,
+  Text,
+  View,
+  Icon,
+  Container,
+} from 'native-base';
 
-import { StyleSheet, TouchableOpacity, KeyboardAvoidingView} from 'react-native'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    }
-})
+  root: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    width: '95%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontWeight: '200',
+    letterSpacing: 2,
+  },
+  form: {
+    width: '100%',
+  },
+  button: {
+    marginTop: 20,
+  },
+  icon: {
+    display: 'flex',
+    width: '100%',
+    alignItems: 'flex-start',
+  },
+});
 
-import firebase from '../server/firebaseconfig'
-import { getUser } from "../store/"
-import { connect } from 'react-redux'
+import firebase from '../server/firebaseconfig';
+import { getUser } from '../store/';
+import { connect } from 'react-redux';
 
 class SignUpScreen extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            phone: '',
-            password: '',
-            username: '',
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      password: '',
+      username: '',
+    };
+  }
 
-    signUpUser = (email, password) => {
-        const { getUser } = this.props
-        try {
-            if (this.state.password.length < 6) {
-                alert('Please enter at least 6 characters')
-                return
-            } else {
-                firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .then(user => {
-                        const { firstName, lastName, phone, username } = this.state
-                        firebase
-                            .database()
-                            .ref('users')
-                            .update({
-                                [user.user.uid]: {
-                                    firstName,
-                                    lastName,
-                                    phone,
-                                    email
-                                }
-                            })
-                        firebase
-                            .database()
-                            .ref('profiles')
-                            .update({
-                                [user.user.uid]: {
-                                    imageUrl: 'https://bit.ly/2qQRtn6',
-                                    username
-                                }
-                            })
-                        getUser({ id: user.user.uid })
-                    })
-                this.props.navigation.navigate('Main')
-            }
-        } catch (err) {
-            console.error(err)
-        }
+  signUpUser = (email, password) => {
+    const { getUser } = this.props;
+    try {
+      if (this.state.password.length < 6) {
+        alert('Please enter at least 6 characters');
+        return;
+      } else {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(user => {
+            const { firstName, lastName, phone, username } = this.state;
+            firebase
+              .database()
+              .ref('users')
+              .update({
+                [user.user.uid]: {
+                  firstName,
+                  lastName,
+                  phone,
+                  email,
+                },
+              });
+            firebase
+              .database()
+              .ref('profiles')
+              .update({
+                [user.user.uid]: {
+                  imageUrl: 'https://bit.ly/2qQRtn6',
+                  username,
+                },
+              });
+            getUser({ id: user.user.uid });
+          });
+        this.props.navigation.navigate('Main');
+      }
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    render() {
-        return (
-            <KeyboardAvoidingView style={styles.container} behavior="padding">
-                    <View style={{ marginLeft: 10 }}>
-                        <TouchableOpacity><Icon type='MaterialCommunityIcons' name='arrow-left' onPress={() => this.props.navigation.navigate('Login')} /></TouchableOpacity>
-                    </View>
-                    <Form>
-                        <Item floatingLabel>
-                            <Label>Username</Label>
-                            <Input
-                                autoCorrect={false}
-                                autoCapitalize='none'
-                                value={this.state.username}
-                                onChangeText={(username) => this.setState({ username })}
-                            />
-                        </Item >
-                        <Item floatingLabel>
-                            <Label>First Name</Label>
-                            <Input
-                                value={this.state.firstName}
-                                autoCorrect={false}
-                                onChangeText={(firstName) => this.setState({ firstName })}
-                            />
-                        </Item >
-                        <Item floatingLabel>
-                            <Label>Last Name</Label>
-                            <Input
-                                value={this.state.lastName}
-                                autoCorrect={false}
-                                onChangeText={(lastName) => this.setState({ lastName })}
-                            />
-                        </Item >
-                        <Item floatingLabel>
-                            <Label>Email</Label>
-                            <Input
-                                keyboardType='email-address'
-                                value={this.state.email}
-                                autoCorrect={false}
-                                autoCapitalize='none'
-                                onChangeText={(email) => this.setState({ email })}
-                            />
-                        </Item >
-                        <Item floatingLabel>
-                            <Label>Phone</Label>
-                            <Input
-                                returnKeyType='done'
-                                keyboardType='phone-pad'
-                                maxLength={11}
-                                dataDetectorTypes='phoneNumber'
-                                value={this.state.phone}
-                                autoCorrect={false}
-                                autoCapitalize='none'
-                                onChangeText={(phone) => this.setState({ phone })}
-                            />
-                        </Item >
-                        <Item floatingLabel>
-                            <Label>Password</Label>
-                            <Input
-                                value={this.state.password}
-                                secureTextEntry={true}
-                                autoCapitalize="none"
-                                autoCorrect={false}
-                                onChangeText={(password) => this.setState({ password })}
-                            />
-                        </Item>
-                        <Button
-                            full
-                            rounded
-                            style={{ marginTop: 10, backgroundColor: 'steelblue' }}
-                            onPress={() => this.signUpUser(this.state.email, this.state.password)}
-                        >
-                            <Text>Sign Up</Text>
-                        </Button>
-                    </Form>
-            </KeyboardAvoidingView>
-        )
-    }
+  render() {
+    return (
+      <Container style={styles.root}>
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+          <View style={styles.icon}>
+            <TouchableOpacity>
+              <Icon
+                type="MaterialCommunityIcons"
+                name="arrow-left"
+                onPress={() => this.props.navigation.navigate('Login')}
+              />
+            </TouchableOpacity>
+          </View>
+          <Form style={styles.form}>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                value={this.state.username}
+                onChangeText={username => this.setState({ username })}
+              />
+            </Item>
+            <Item floatingLabel>
+              <Label>First Name</Label>
+              <Input
+                value={this.state.firstName}
+                autoCorrect={false}
+                onChangeText={firstName => this.setState({ firstName })}
+              />
+            </Item>
+            <Item floatingLabel>
+              <Label>Last Name</Label>
+              <Input
+                value={this.state.lastName}
+                autoCorrect={false}
+                onChangeText={lastName => this.setState({ lastName })}
+              />
+            </Item>
+            <Item floatingLabel>
+              <Label>Email</Label>
+              <Input
+                keyboardType="email-address"
+                value={this.state.email}
+                autoCorrect={false}
+                autoCapitalize="none"
+                onChangeText={email => this.setState({ email })}
+              />
+            </Item>
+            <Item floatingLabel>
+              <Label>Phone</Label>
+              <Input
+                returnKeyType="done"
+                keyboardType="phone-pad"
+                maxLength={11}
+                dataDetectorTypes="phoneNumber"
+                value={this.state.phone}
+                autoCorrect={false}
+                autoCapitalize="none"
+                onChangeText={phone => this.setState({ phone })}
+              />
+            </Item>
+            <Item floatingLabel>
+              <Label>Password</Label>
+              <Input
+                value={this.state.password}
+                secureTextEntry={true}
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={password => this.setState({ password })}
+              />
+            </Item>
+            <Button
+              success
+              block
+              style={styles.button}
+              onPress={() =>
+                this.signUpUser(this.state.email, this.state.password)
+              }
+            >
+              <Text style={styles.text}>SIGN UP</Text>
+            </Button>
+          </Form>
+        </KeyboardAvoidingView>
+      </Container>
+    );
+  }
 }
 
-export default connect(null, { getUser })(SignUpScreen)
-
+export default connect(
+  null,
+  { getUser }
+)(SignUpScreen);
