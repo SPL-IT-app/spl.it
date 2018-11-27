9f81dc296e45fb2e180b22223f0844d1e992da6d
Julianne Crawford <julianne.marik@gmail.com>
2018-11-27 12:50:32 -0600

    more styling complete

--- screens/MoreScreen.js
+++ screens/MoreScreen.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { Container, Text, Footer, Button, Icon, Content, FooterTab } from 'native-base'
-import { MyHeader } from '../components'
-import { StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import {AllEvents} from '../screens'
-
-const styles = StyleSheet.create({
-
-});
 
 class MoreScreen extends React.Component {
   static navigationOptions = {
